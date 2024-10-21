86896a93478e366d6b8324eb09395748b161668c
Edwin Saucedo <edwin.saucedo@encora.com>
2024-10-21 17:02:33 -0600

    feat: add new class for pagination

--- src/main/java/com/example/backend_breakable_toy_i_todoapp/controller/ToDoController.java
+++ src/main/java/com/example/backend_breakable_toy_i_todoapp/controller/ToDoController.java
@@ -1,5 +1,6 @@
 package com.example.backend_breakable_toy_i_todoapp.controller;
 
+import com.example.backend_breakable_toy_i_todoapp.model.AllTasksResponse;
 import com.example.backend_breakable_toy_i_todoapp.model.Task;
 import com.example.backend_breakable_toy_i_todoapp.service.TaskService;
 import com.example.backend_breakable_toy_i_todoapp.service.TaskServiceInterface;
@@ -15,16 +16,17 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping(value="/todos")
-public class ToDoController implements TaskServiceInterface {
+public class ToDoController {
     @Autowired TaskService service;
     @RequestMapping
-    public ResponseEntity<List<Task>> getAllTasks(@RequestParam(required = false) String filter, @RequestParam(defaultValue = "1") Integer page) {
+    public ResponseEntity<AllTasksResponse> getAllTasks(@RequestParam(defaultValue = "") String name, @RequestParam(defaultValue = "all") String priority, @RequestParam(defaultValue = "all") String status, @RequestParam(defaultValue = "1") Integer page) {
         try {
-            return new ResponseEntity<>(service.getAllTasks(filter, page), HttpStatus.OK);
+            return new ResponseEntity<>(service.getAllTasks(status, name, priority, page), HttpStatus.OK);
         } catch (RuntimeException e) {
             // If any error return an empty list to avoid rendering errors
-            return new ResponseEntity<>(new ArrayList<Task>(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new AllTasksResponse(null, 0), HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/com/example/backend_breakable_toy_i_todoapp/service/TaskService.java
+++ src/main/java/com/example/backend_breakable_toy_i_todoapp/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.example.backend_breakable_toy_i_todoapp.service;
 
 import com.example.backend_breakable_toy_i_todoapp.dao.TaskDAO;
+import com.example.backend_breakable_toy_i_todoapp.model.AllTasksResponse;
 import com.example.backend_breakable_toy_i_todoapp.model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,54 +12,51 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
-public class TaskService {
+public class TaskService implements  TaskServiceInterface{
     @Autowired
     private TaskDAO taskDAO;
     protected final int PAGE_SIZE = 10;
     protected final List<String> priorities = List.of("high", "medium", "low");
 
-    public List<Task> getAllTasks(String filter, Integer page){
-        List<String> filters = List.of(filter.split("&"));
+    public AllTasksResponse getAllTasks(String status, String name, String priority, Integer page){
         List <Task> tasks = new ArrayList<Task>(taskDAO.getAll().values());
-        for(String filtered : filters){
-            if(filtered.contains("status")){
-                // Filter by status
-                if(Arrays.asList(filtered.split("=")).contains("done")){
-                    tasks = tasks.stream().filter(task -> task.getDoneDate() != null).toList();
-                }
-                else if(Arrays.asList(filtered.split("=")).contains("undone")) {
-                    tasks = tasks.stream().filter(task -> task.getDoneDate() == null).toList();
-                }
+        int size = tasks.size();
+        if(status != null){
+            // Filter by status
+            if(status.equals("done")){
+                tasks = tasks.stream().filter(task -> task.getDoneDate() != null).toList();
             }
-            if(filtered.contains("priority")){
-                // Filter by priority
-                if(Arrays.asList(filtered.split("=")).contains("high")){
-                    tasks = tasks.stream().filter(task -> task.getPriority().equals("high")).toList();
-                }
-                else if(Arrays.asList(filtered.split("=")).contains("medium")) {
-                    tasks = tasks.stream().filter(task -> task.getPriority().equals("medium")).toList();
-                }
-                else if(Arrays.asList(filtered.split("=")).contains("low")) {
-                    tasks = tasks.stream().filter(task -> task.getPriority().equals("low")).toList();
-                }
+            else if(status.equals("undone")) {
+                tasks = tasks.stream().filter(task -> task.getDoneDate() == null).toList();
             }
-            if(filtered.contains("name")){
-               String name = Arrays.asList(filtered.split("=")).get(1);
-               if(name != null){
-                   // Filter if contains similar name
-                   tasks = tasks.stream()
-                           .filter(task -> task.getName().toLowerCase().contains(name.toLowerCase()))
-                           .toList();
-               }
+        }
+        if(priority != null){
+            // Filter by priority
+            if(priority.equals("high")){
+                tasks = tasks.stream().filter(task -> task.getPriority().equals("high")).toList();
             }
+            else if(priority.equals("medium")) {
+                tasks = tasks.stream().filter(task -> task.getPriority().equals("medium")).toList();
+            }
+            else if(priority.equals("low")) {
+                tasks = tasks.stream().filter(task -> task.getPriority().equals("low")).toList();
+            }
+        }
+        if(name != null){
+           tasks = tasks.stream()
+                   .filter(task -> task.getName().toLowerCase().contains(name.toLowerCase()))
+                   .toList();
+
         }
         // Used for autoincremental index;
         AtomicInteger index = new AtomicInteger();
         // Get autoincrement index and filter by pagination
-        return tasks.stream()
+        List<Task> filtered = tasks.stream()
                 .map(el -> index.getAndIncrement())
                 .filter(i -> (i < (page * PAGE_SIZE) && i >= ((page - 1) * PAGE_SIZE)))
                 .map(tasks::get).toList();
+        AllTasksResponse tasksResponse  = new AllTasksResponse(filtered, size);
+        return tasksResponse;
     }
 
     public Task getTaskById(UUID id){
@@ -82,9 +80,9 @@ public class TaskService {
         return  new ResponseEntity<String>(taskDAO.addTask(newTask).toString(), HttpStatus.OK);
     }
     public ResponseEntity<String> updateTask(UUID id, Task updatedTask){
-        if(updatedTask.hasRequiredFields()){
-            return new ResponseEntity<>("Provided task has missing properties", HttpStatus.BAD_REQUEST);
-        }
+//        if(updatedTask.hasRequiredFields()){
+//            return new ResponseEntity<>("Provided task has missing properties", HttpStatus.BAD_REQUEST);
+//        }
         if(!priorities.contains(updatedTask.getPriority())){
             return  new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
         }

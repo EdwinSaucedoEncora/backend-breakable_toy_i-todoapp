84e08dfad7395c5a88e0aa89d5d71eb866c9294a
Edwin Saucedo <edwin.saucedo@encora.com>
2024-10-21 23:12:36 -0600

    feat: add metrics endpoint
    - Add average details model
    - Add average details calculable method into dao and obtained by service
    - Add method to calculate diff days between today and dueDate to calculate average

--- src/main/java/com/example/backend_breakable_toy_i_todoapp/controller/ToDoController.java
+++ src/main/java/com/example/backend_breakable_toy_i_todoapp/controller/ToDoController.java
@@ -1,18 +1,14 @@
 package com.example.backend_breakable_toy_i_todoapp.controller;
 
 import com.example.backend_breakable_toy_i_todoapp.model.AllTasksResponse;
+import com.example.backend_breakable_toy_i_todoapp.model.AverageDetails;
 import com.example.backend_breakable_toy_i_todoapp.model.Task;
 import com.example.backend_breakable_toy_i_todoapp.service.TaskService;
-import com.example.backend_breakable_toy_i_todoapp.service.TaskServiceInterface;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -50,14 +46,19 @@ public class ToDoController {
         return service.addTask(newTask);
     }
 
-    @RequestMapping(value = "{id}/done", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}/done", method = RequestMethod.PUT)
     public ResponseEntity<String> setDoneDateById(@PathVariable UUID id){
         return service.setDoneDateById(id);
     }
 
-    @RequestMapping(value = "{id}/undone", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}/undone", method = RequestMethod.PUT)
     public ResponseEntity<String> unsetDoneDateById(@PathVariable UUID id){
         return service.unsetDoneDateById(id);
+    }
+
+    @RequestMapping(value = "/metrics", method = RequestMethod.GET)
+    public ResponseEntity<AverageDetails> getAverageDetails(){
+        return service.getAverageDetails();
     }
 }
 
